608c9c67fc35abe6fba29592de047bdcdecc5103
ida <abdourahamane.ida.etu@esmt.sn>
2023-05-01 05:00:31 +0200

    mise a jour des  duj logo et tout  par ida et manel comme le travaille orécédet

--- app/src/androidTest/java/com/example/test1_projet/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/test1_projet/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package com.example.test1_projet
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
