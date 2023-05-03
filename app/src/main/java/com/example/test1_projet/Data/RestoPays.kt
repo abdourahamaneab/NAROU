e8be5ae9c3706802af31762776864354834fde41
ida <abdourahamane.ida.etu@esmt.sn>
2023-05-03 05:55:19 +0200

    pour le travaille effectuer referer vous au readme pour pouvoir nous noter parce que on a eu des problemes qui y seront expliquer merci de votre compréhension

--- app/src/main/java/com/example/test1_projet/Data/RestoPays.kt
+++ app/src/main/java/com/example/test1_projet/Data/RestoPays.kt
@@ -5,21 +5,25 @@ import com.example.test1_projet.R
 
 class RestoPays {
     val france = listOf(
-        restoModels("resto1", R.drawable.agra_taj_mahal),
-        restoModels("resto2", R.drawable.nyc),
-        restoModels("resto3", R.drawable.mexico),
-        restoModels("resto4", R.drawable.home_bg),
-        restoModels("resto5", R.drawable.agra_taj_mahal),
-        restoModels("resto6", R.drawable.nyc),
+        restoModels("L'Ambroisie", R.drawable.fran_resto_ambro),
+        restoModels("La Tour d'Argent", R.drawable.fran_resto2_tur_argt),
+        restoModels("Le Jules Verne ", R.drawable.fran_resto3_jul_vern),
     )
 
     val italie = listOf(
-        restoModels("resto1", R.drawable.agra_taj_mahal),
-        restoModels("resto2", R.drawable.nyc),
-        restoModels("resto3", R.drawable.mexico),
-        restoModels("resto4", R.drawable.home_bg),
-        restoModels("resto5", R.drawable.agra_taj_mahal),
-        restoModels("resto6", R.drawable.nyc),
+        restoModels("Osteria Francescana", R.drawable.italie_restau_oste),
+        restoModels("La Pergola ", R.drawable.italie_restau_pergola),
+        restoModels("Piazza Duomo ", R.drawable.italie_restau_piazza),
+    )
+    val chine = listOf(
+        restoModels("Quanjude ", R.drawable.chine_restau_quanju),
+        restoModels("Din Tai Fung", R.drawable.chine_restau_din_haidilao),
+        restoModels("Haidilao Hot Pot", R.drawable.chine_restau_din_haidilao),
+    )
+    val USA = listOf(
+        restoModels(" Eleven Madison Park ", R.drawable.usa_restau_eleven),
+        restoModels(" Alinea ", R.drawable.usa_restau_aline),
+        restoModels(" French Laundry ", R.drawable.usa_restau_french),
     )
 
     // recupere les resto par nom
@@ -27,6 +31,8 @@ class RestoPays {
         return when (name) {
             "france" -> france
             "italie" -> italie
+            "chine" -> chine
+            "usa" -> USA
             else -> france
         }
     }
